0b3bc56b3ea4b5864a8c26c15ead4e9ed9424bd8
Tanya Novosiltseva <tnovosiltsevaa@mail.ru>
2022-05-14 14:21:03 -0400

    updated

---
+++ data.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+text1 = { 
+        'Text': "The Pentagon denied providing 'specific targeting information' to Ukraine to sink the Moskva, a Russian guided-missile cruiser that was the flagship of Moscow's fleet in the Black Sea.",
+        'Source': 'CNN',
+        'Date': datetime(2022, 3, 10, 16, 0)
+    }
+
+text2 = { 
+    'Text': "Ankara, which can block new members to alliance, accuses Nordic countries of harboring Kurdish ‘terror’ groups, as they seek deterrent against Russia",
+    'Source': 'The Times of Israel',
+    'Date': datetime(2022, 5, 14, 11, 1)
+}
+
+text3 = { 
+    'Text': "Former employees, backed by the Whistleblower Aid charity, say the company intentionally 'over-blocked' Australian pages at a critical time to gain leverage over the Australian government.",
+    'Source': 'Yahoo News',
+    'Date': datetime(2022, 3, 12, 9, 45)
+}
+
+text_example = [text1, text2, text3]
